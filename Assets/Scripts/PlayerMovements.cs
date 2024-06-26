4e88f09e8f0e25e4ab75913309a9497f9dac1c3b
Wind395 <phongntph49930@gmail.com>
2024-06-26 14:13:10 +0700

    Wind Added Animation (Just Basic)

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -1,25 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovements : MonoBehaviour
 {
 
     private Rigidbody2D rb;
+    private Animator animator;
     private float moveH, moveV;
     [SerializeField] private float moveSpeed = 1.0f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
-        moveH = Input.GetAxis("Horizontal") * moveSpeed;
-        moveV = Input.GetAxis("Vertical") * moveSpeed;
-        rb.velocity = new Vector2(moveH, moveV);
+        rb.velocity = new Vector2(moveH * moveSpeed, moveV * moveSpeed);
 
         Vector2 direction = new Vector2(moveH, moveV);
+
+        animator.SetTrigger(AnimationEffects());
+    }
+
+    string AnimationEffects()
+    {
+        if (moveH > 0)
+        {
+            return "E";
+        }
+        if (moveH < 0)
+        {
+            return "W";
+        }
+        if (moveV > 0)
+        {
+            return "N";
+        }
+        if (moveV < 0)
+        {
+            return "S";
+        }
+        return null;
+    }
+
+    public void Move(InputAction.CallbackContext callbackContext)
+    {
+        moveH = callbackContext.ReadValue<Vector2>().x;
+        moveV = callbackContext.ReadValue<Vector2>().y;
     }
 }
